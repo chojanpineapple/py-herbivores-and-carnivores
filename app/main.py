b28e815c1f77f0124c07e1e4be07ec265965a293
Johan Rangel <johan.apr99@gmail.com>
2025-12-30 09:32:58 +0000

    test done1

--- app/main.py
+++ app/main.py
@@ -1,29 +1,32 @@
 class Animal:
-    # this is my test
     alive = []
-    def __init__(self, name: str) -> None:
+    def __init__(self,
+                 name: str) -> None:
         self.name = name
         self.health = 100
         self.hidden = False
         Animal.alive.append(self)
     
     def __repr__(self):
-        return (f"Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}")
+        return ("{"+f"Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}"+"}")
 
 class Herbivore(Animal):
-    def __init__(self, name: str) -> None:
+    def __init__(self,
+                 name: str) -> None:
         super().__init__(name)
 
     def hide(self):
-        self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def __init__(self, name: str) -> None:
+    def __init__(self,
+                 name: str) -> None:
         super().__init__(name)
     
-    def bite(self, animal: Animal) -> None:
-        if animal in Animal.alive and not animal.hidden and animal.Herbivore:
+    def bite(self,
+             animal: Animal) -> None:
+        if animal in Animal.alive and not animal.hidden and isinstance(animal, Herbivore):
             animal.health -= 50
             if animal.health <= 0:
                 Animal.alive.remove(animal)
